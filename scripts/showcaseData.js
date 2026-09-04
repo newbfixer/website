31a2cd194b42aff5fd4419538e5e0f90d61b5bd0
newbfixer <stereo_genic@msn.com>
2026-09-04 02:23:22 +0100

    Add files via upload

---
+++ scripts/showcaseData.js
@@ -0,0 +1,37 @@
+const showcaseData = [
+    {		carModel: "Mercedes AMG GT3",
+    		type: "Custom Livery",
+        team: "Restless Racing Team",
+        images: [
+        "images/showcase/rest1.jpg", 
+        "images/showcase/rest2.jpg", 
+        "images/showcase/rest3.jpg", 
+        "images/showcase/rest4.jpg", 
+        "images/showcase/rest5.jpg", 
+        "images/showcase/rest6.jpg"]
+    },
+    {
+        carModel: "Mercedes AMG GT3",
+        type: "Reproduction",
+        team: "TAXI Racing Team",
+        images: [
+        "images/showcase/taxi1.jpg", 
+        "images/showcase/taxi2.jpg", 
+        "images/showcase/taxi3.jpg", 
+        "images/showcase/taxi4.jpg", 
+        "images/showcase/taxi5.jpg", 
+        "images/showcase/taxi6.jpg"]
+    },
+    {
+        carModel: "Mercedes AMG GT3",
+        type: "Custom Livery",
+        team: "Tornado Racing",
+        images: [
+        "images/showcase/tor1.jpg", 
+        "images/showcase/tor2.jpg", 
+        "images/showcase/tor3.jpg", 
+        "images/showcase/tor4.jpg", 
+        "images/showcase/tor5.jpg", 
+        "images/showcase/tor6.jpg"]
+    }
+];
