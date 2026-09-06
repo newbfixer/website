37dc80f87e06d5188dc7c27fc8f754db3878b0af
newbfixer <stereo_genic@msn.com>
2026-09-07 00:45:16 +0100

    Add files via upload

--- scripts/showcaseData.js
+++ scripts/showcaseData.js
@@ -3,35 +3,143 @@ const showcaseData = [
     		type: "Custom Livery",
         team: "Restless Racing Team",
         images: [
-        "images/showcase/rest1.jpg", 
-        "images/showcase/rest2.jpg", 
-        "images/showcase/rest3.jpg", 
-        "images/showcase/rest4.jpg", 
-        "images/showcase/rest5.jpg", 
-        "images/showcase/rest6.jpg"]
+        "https://i.ibb.co/HpDrZ013/rest1.jpg", 
+        "https://i.ibb.co/Ps7V4sV2/rest2.jpg",
+				"https://i.ibb.co/HLZ4ktdJ/rest3.jpg",
+				"https://i.ibb.co/60pLnTyk/rest4.jpg",
+				"https://i.ibb.co/XfZSJH2d/rest5.jpg",
+				"https://i.ibb.co/84syNb03/rest6.jpg"]
     },
     {
         carModel: "Mercedes AMG GT3",
         type: "Reproduction",
         team: "TAXI Racing Team",
         images: [
-        "images/showcase/taxi1.jpg", 
-        "images/showcase/taxi2.jpg", 
-        "images/showcase/taxi3.jpg", 
-        "images/showcase/taxi4.jpg", 
-        "images/showcase/taxi5.jpg", 
-        "images/showcase/taxi6.jpg"]
+        "https://i.ibb.co/MxYy67v5/taxi1.jpg",
+				"https://i.ibb.co/B28gc4Kd/taxi2.jpg",
+				"https://i.ibb.co/tTcDfNhj/taxi3.jpg",
+				"https://i.ibb.co/5XYrQDgK/taxi4.jpg",
+				"https://i.ibb.co/vCQ1TZ0x/taxi5.jpg",
+				"https://i.ibb.co/gZ50Ppkq/taxi6.jpg"]
     },
     {
         carModel: "Mercedes AMG GT3",
         type: "Custom Livery",
         team: "Tornado Racing",
         images: [
-        "images/showcase/tor1.jpg", 
-        "images/showcase/tor2.jpg", 
-        "images/showcase/tor3.jpg", 
-        "images/showcase/tor4.jpg", 
-        "images/showcase/tor5.jpg", 
-        "images/showcase/tor6.jpg"]
+        "https://i.ibb.co/MyDY5KL2/tor1.jpg",
+				"https://i.ibb.co/6hVp9Tq/tor2.jpg",
+				"https://i.ibb.co/60TbpXMH/tor3.jpg",
+				"https://i.ibb.co/21vKmXby/tor4.jpg",
+				"https://i.ibb.co/NdjVnHKq/tor5.jpg",
+				"https://i.ibb.co/SXhXv3mv/tor6.jpg"]
+    },
+    {
+        carModel: "McLaren 720s GT3",
+        type: "Reproduction",
+        team: "Williams Esports",
+        images: [
+        "https://i.ibb.co/4ZBQb1Cw/chillblast0.jpg",
+				"https://i.ibb.co/Zp4v9BHq/chillblast2.jpg",
+				"https://i.ibb.co/jPpS7xj7/chillblast3.jpg",
+				"https://i.ibb.co/n8jbcbn1/chillblast4.jpg",
+				"https://i.ibb.co/MxGSMQy0/chillblast5.jpg",
+				"https://i.ibb.co/Q0KqgW4/chillblast6.jpg"]
+    },
+    {
+        carModel: "McLaren 720s GT3 Evo",
+        type: "Custom livery",
+        team: "Bruing Thru",
+        images: [
+        "https://i.ibb.co/YFNTfNv8/bruthru0.jpg",
+				"https://i.ibb.co/ccXzt4gW/bruthru2.jpg",
+				"https://i.ibb.co/Df8WR5Pg/bruthru3.jpg",
+				"https://i.ibb.co/Mk3sNYCF/bruthru4.jpg",
+				"https://i.ibb.co/Y72phn1X/bruthru5.jpg",
+				"https://i.ibb.co/zTxcxPj7/bruthru6.jpg"]
+    },
+    {
+        carModel: "Lamborghini Huracan GT3 Evo2",
+        type: "Custom livery",
+        team: "CornHub",
+        images: [
+        "https://i.ibb.co/bRD5qfrV/cornhub0.jpg",
+				"https://i.ibb.co/WNS0HJdR/cornhub2.jpg",
+				"https://i.ibb.co/8LYYG4VT/cornhub3.jpg",
+				"https://i.ibb.co/Kprf5DYP/cornhub4.jpg",
+				"https://i.ibb.co/mCPfks3t/cornhub5.jpg",
+				"https://i.ibb.co/4ZQNJ6Vk/cornhub6.jpg"]
+    },
+    {
+        carModel: "Porsche 992 GT3R",
+        type: "Custom livery",
+        team: "Cryonics Racing",
+        images: [
+        "https://i.ibb.co/rKv50BZP/cryonics0.jpg",
+				"https://i.ibb.co/Y7fZ721t/cryonics2.jpg",
+				"https://i.ibb.co/KpDqD7pz/cryonics3.jpg",
+				"https://i.ibb.co/v405MTc8/cryonics4.jpg",
+				"https://i.ibb.co/bRmxB4rB/cryonics5.jpg",
+				"https://i.ibb.co/1t15jt5Q/cryonics6.jpg"]
+    },
+    {
+        carModel: "Chevrolet Camaro GT4R",
+        type: "Reproduction",
+        team: "HOP Racing",
+        images: [
+        "https://i.ibb.co/1YQ6V8Y6/dewalt0.jpg",
+				"https://i.ibb.co/d4jft0TC/dewalt2.jpg",
+				"https://i.ibb.co/2YCJXffc/dewalt3.jpg",
+				"https://i.ibb.co/NHTgy32/dewalt4.jpg",
+				"https://i.ibb.co/Qj80v8wD/dewalt5.jpg",
+				"https://i.ibb.co/fVJgqKwm/dewalt6.jpg"]
+    },
+    {
+        carModel: "McLaren 720s GT3 Evo",
+        type: "Custom Livery",
+        team: "Holy Energy",
+        images: [
+        "https://i.ibb.co/YGs34S3/holyenergy0.jpg",
+				"https://i.ibb.co/BHYsZq05/holyenergy2.jpg",
+				"https://i.ibb.co/cKdQSyzy/holyenergy3.jpg",
+				"https://i.ibb.co/chV8nfZZ/holyenergy4.jpg",
+				"https://i.ibb.co/8gQbqbs6/holyenergy5.jpg",
+				"https://i.ibb.co/G39cGrcB/holyenergy6.jpg"]
+    },
+    {
+        carModel: "Ferrari 296 GT3",
+        type: "Custom Livery",
+        team: "Vecchia Romagna",
+        images: [
+        "https://i.ibb.co/M5NvNhRW/romagna0.jpg",
+				"https://i.ibb.co/k6WNXLZt/romagna2.jpg",
+				"https://i.ibb.co/TxKDhJ58/romagna3.jpg",
+				"https://i.ibb.co/PGZnxTHh/romagna4.jpg",
+				"https://i.ibb.co/qMNnQjJp/romagna5.jpg",
+				"https://i.ibb.co/mV2y5nNs/romagna6.jpg"]
+    },
+    {
+        carModel: "Ferrari 296 GT3",
+        type: "Reproduction",
+        team: "Agip",
+        images: [
+        "https://i.ibb.co/39nb9G2S/agip0.jpg",
+				"https://i.ibb.co/4wYHXnpy/agip2.jpg",
+				"https://i.ibb.co/BVwgSjQm/agip3.jpg",
+				"https://i.ibb.co/hRLcBbxx/agip4.jpg",
+				"https://i.ibb.co/6Jp0VBcG/agip5.jpg",
+				"https://i.ibb.co/gZLSXZDz/agip6.jpg"]
+    },
+    {
+        carModel: "Ferrari 296 GT3",
+        type: "Custom Livery",
+        team: "Shell V-Power",
+        images: [
+        "https://i.ibb.co/tPPT9NPt/shellferarri0.jpg",
+				"https://i.ibb.co/XZMZpmrF/shellferrari2.jpg",
+				"https://i.ibb.co/sv0hNzQY/shellferrari3.jpg",
+				"https://i.ibb.co/8gCQMW7J/shellferrari4.jpg",
+				"https://i.ibb.co/bjc3tKvw/shellferrari5.jpg",
+				"https://i.ibb.co/zVVJCgRT/shellferrari6.jpg"]
     }
 ];
